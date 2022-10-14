30c6f7f73aef7b74aa05e65bc6412aa43bc663c1
亦阳 <51779447+yiyanger@users.noreply.github.com>
2022-10-14 10:28:22 +0800

    Update status.sh

--- status.sh
+++ status.sh
@@ -239,8 +239,8 @@ function get_status() {
 function install_client() {
     echo -e "${Info} 下载 ${arch} 二进制文件"
     [ -f "/tmp/stat_client" ] || get_status
+    client_dir=/usr/local/ServerStatus/client
     mkdir -p ${client_dir}
-    mkdir -p /usr/local/ServerStatus/client
     mv /tmp/stat_client /usr/local/ServerStatus/client/stat_client
     chmod +x /usr/local/ServerStatus/client/stat_client
     input_upm
